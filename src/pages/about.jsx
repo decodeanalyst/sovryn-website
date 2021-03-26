62b6c7bb7afe9becc136f201962a2fc216e77fc0
Jarrar Shah <jarrarhaidershah1@gmail.com>
2021-03-27 01:25:29 +0500

    /blog-article /home header.jsx added

---
+++ src/pages/about.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "../components/header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "/src/css/header.css";
+
+const AboutPage = () => (
+  <main>
+      <Header></Header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                {/* <h1 class="text-center p-5 text-danger">About the Author</h1>
+                <p>Welcome to my Gatsby site.</p> */}
+        </div>
+        </div>
+        </div>
+  </main>
+)
+export default AboutPage
