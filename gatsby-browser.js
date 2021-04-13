280852b632e72728f749cdce8684fa608a3f42b7
Nidal Malik <artbat-82@users.noreply.github.com>
2021-04-13 23:31:39 +0500

    added round FontAwesome icons to SocialFooterLinks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,12 @@
+// import javascript and bootstrap
 import "bootstrap/scss/bootstrap.scss"
 import 'jquery/dist/jquery.min.js'
 import "bootstrap/dist/js/bootstrap.min.js"
 import 'popper.js/dist/popper.min'
+
+// importing fontawesome library components
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, faDiscord, faTelegram, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { fas, faCircle } from '@fortawesome/free-solid-svg-icons'
+
+library.add( fab, faDiscord, fas, faCircle, faTelegram, faGithub, faTwitter ); // adding imported modules to library to be used with FontAwesomeIcon components
